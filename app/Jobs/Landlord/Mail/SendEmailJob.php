fe13698486831c2ba31d962fa3907505a558e8d5
César Luis García Castro <cesarluisgarciacastro@gmail.com>
2023-09-18 17:05:16 -0600

    Se agrega envio de emails mediante Queue
    -Se agrega proceso de envio de correos base para el envio individual y masivo de notificaciones.
    -Se agrega JOB para el manejo de colas de envio de emails mediante base de datos.

---
+++ app/Jobs/Landlord/Mail/SendEmailJob.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs\Landlord\Mail;
+
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailContainer;
+use Illuminate\Bus\Queueable;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $details;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $i = 1;
+        while ($i <= 10) {
+            $email = new EmailContainer();
+            Mail::to($this->details['email'])->send($email);
+            $i++;
+        }
+    }
+}
